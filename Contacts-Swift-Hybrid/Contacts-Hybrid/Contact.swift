152cd914d6274553f1d394a91c3d91aa29e81dab
Claudia Maciel <clc_80@yahoo.com>
2020-08-04 18:40:27 -0700

    Implemented contact class

--- Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+class Contact {
+    
+    var name: String
+    var relationships: Strng?
+    
+    init(name: String, relationship: String? = nil) {
+        self.name = name
+        self.relationships = relationships
+    }
+}
