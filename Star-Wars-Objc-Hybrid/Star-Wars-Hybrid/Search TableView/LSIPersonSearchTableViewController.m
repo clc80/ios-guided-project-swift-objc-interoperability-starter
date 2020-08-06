b372590e0310b3bc756c105272c400e69d578463
Claudia Maciel <clc_80@yahoo.com>
2020-08-05 19:49:15 -0700

    Integrated the cell

--- Star-Wars-Objc-Hybrid/Star-Wars-Hybrid/Search TableView/LSIPersonSearchTableViewController.m
+++ Star-Wars-Objc-Hybrid/Star-Wars-Hybrid/Search TableView/LSIPersonSearchTableViewController.m
@@ -27,22 +27,25 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
     
 	// TODO: Implement number of rows
 	
-	return 0;
+    return self.people.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
 
 	
 	// TODO: Implement a custom cell named PersonTableViewCell.swift
 	
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PersonCell" forIndexPath:indexPath];
+	LSIPersonTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PersonCell" forIndexPath:indexPath];
     
 	// TODO: Set the cell to the current Person object
-	
+    LSIPerson *person =[self.people objectAtIndex: indexPath.row];
+    cell.person = person;
 	
     return cell;
 }
